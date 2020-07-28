e1744678580451377c9e3f8e59ee2aa79b01ab84
Yigit Mersin <yigitmersin@gmail.com>
2020-07-28 12:17:19 +0300

    Add tic tac toe user interface design with prompts asking for user input

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,28 @@
 #!/usr/bin/env ruby
-puts "Hello World!"
+
+  def board 
+    puts " 1 | 2 | 3 "
+    puts "-----------"
+    puts " 4 | 5 | 6 "
+    puts "-----------"
+    puts " 7 | 8 | 9 "
+  end
+
+puts "Enter Player-1 name"
+player_1 = gets.chomp
+puts "Enter Player-2 name"
+player_2 = gets.chomp
+
+puts "#{player_1} turn"
+puts "Pick any avaliable number 1 to 9"
+board
+player_1_choice = gets.chomp
+
+puts "#{player_2} turn"
+puts "Pick any avaliable number 1 to 9"
+board
+player_2_choice = gets.chomp
+
+puts "#{player_1} win!"
+puts "#{player_2} win!"
+puts "#{player_1} &  #{player_2} draw"
