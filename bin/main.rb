b93f170872d57cdc4616643f362d6735186b5273
Yigit Mersin <yigitmersin@gmail.com>
2020-07-28 15:29:33 +0300

    Create game_logic branch

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-
+class TicTacToe
+    
 def board
   puts ' 1 | 2 | 3 '
   puts '-----------'
@@ -8,10 +9,12 @@ def board
   puts ' 7 | 8 | 9 '
 end
 
+def welcome    
 puts 'Enter Player-1 name'
 player1 = gets.chomp
 puts 'Enter Player-2 name'
 player2 = gets.chomp
+end
 
 player1_turns = []
 player2_turns = []
@@ -32,3 +35,14 @@ end
 puts "#{player1} win!"
 puts "#{player2} win!"
 puts "#{player1} &  #{player2} draw"
+
+def start
+    welcome
+end
+
+end
+
+
+
+game = TicTacToe.new
+game.start
