4721552561fa13db1ac8e72e757d32167d884eff
jacobrees <jacob.rees97@yahoo.com>
2020-07-28 11:49:38 +0100

    Create loop for repeating actions to play game in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -8,21 +8,23 @@
     puts " 7 | 8 | 9 "
   end
 
+
 puts "Enter Player-1 name"
 player_1 = gets.chomp
 puts "Enter Player-2 name"
 player_2 = gets.chomp
 
-puts "#{player_1} turn"
-puts "Pick any avaliable number 1 to 9"
-board
-player_1_choice = gets.chomp
+while true
+    puts "#{player_1} turn"
+    puts "Pick any avaliable number 1 to 9"
+    board
+    player_1_choice = gets.chomp
 
-puts "#{player_2} turn"
-puts "Pick any avaliable number 1 to 9"
-board
-player_2_choice = gets.chomp
-
+    puts "#{player_2} turn"
+    puts "Pick any avaliable number 1 to 9"
+    board
+    player_2_choice = gets.chomp
+end
 puts "#{player_1} win!"
 puts "#{player_2} win!"
 puts "#{player_1} &  #{player_2} draw"
