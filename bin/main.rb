1792f03d75a9eacd9538bab1c6af8e750d2a7425
jacobrees <jacob.rees97@yahoo.com>
2020-07-28 12:10:12 +0100

    Fix linter errors within main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -1,30 +1,35 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
-  def board 
-    puts " 1 | 2 | 3 "
-    puts "-----------"
-    puts " 4 | 5 | 6 "
-    puts "-----------"
-    puts " 7 | 8 | 9 "
-  end
-
-
-puts "Enter Player-1 name"
-player_1 = gets.chomp
-puts "Enter Player-2 name"
-player_2 = gets.chomp
-
-while true
-    puts "#{player_1} turn"
-    puts "Pick any avaliable number 1 to 9"
-    board
-    player_1_choice = gets.chomp
-
-    puts "#{player_2} turn"
-    puts "Pick any avaliable number 1 to 9"
-    board
-    player_2_choice = gets.chomp
+def board
+  puts ' 1 | 2 | 3 '
+  puts '-----------'
+  puts ' 4 | 5 | 6 '
+  puts '-----------'
+  puts ' 7 | 8 | 9 '
 end
-puts "#{player_1} win!"
-puts "#{player_2} win!"
-puts "#{player_1} &  #{player_2} draw"
+
+puts 'Enter Player-1 name'
+player1 = gets.chomp
+puts 'Enter Player-2 name'
+player2 = gets.chomp
+
+player1_turns = []
+player2_turns = []
+
+loop do
+  puts "#{player1} turn"
+  puts 'Pick any avaliable number 1 to 9'
+  board
+  player1_turn = gets.chomp
+  player1_turns << player1_turn
+
+  puts "#{player2} turn"
+  puts 'Pick any avaliable number 1 to 9'
+  board
+  player2_turn = gets.chomp
+  player2_turns << player2_turn
+end
+puts "#{player1} win!"
+puts "#{player2} win!"
+puts "#{player1} &  #{player2} draw"
