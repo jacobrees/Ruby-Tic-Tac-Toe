edab16bb7af9bb7e944cb4af7d6bea38dfe63b44
jacobrees <jacob.rees97@yahoo.com>
2020-07-28 13:58:44 +0100

    Organised code within main.rb file into a class and methods

--- bin/main.rb
+++ bin/main.rb
@@ -1,48 +1,41 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 class TicTacToe
-    
-def board
-  puts ' 1 | 2 | 3 '
-  puts '-----------'
-  puts ' 4 | 5 | 6 '
-  puts '-----------'
-  puts ' 7 | 8 | 9 '
-end
+  def welcome
+    puts 'Enter Player-1 name'
+    @player1 = gets.chomp
+    puts 'Enter Player-2 name'
+    @player2 = gets.chomp
+  end
 
-def welcome    
-puts 'Enter Player-1 name'
-player1 = gets.chomp
-puts 'Enter Player-2 name'
-player2 = gets.chomp
-end
+  def board
+    puts ' 1 | 2 | 3 '
+    puts '-----------'
+    puts ' 4 | 5 | 6 '
+    puts '-----------'
+    puts ' 7 | 8 | 9 '
+  end
 
-player1_turns = []
-player2_turns = []
+  def play
+    loop do
+      puts "#{@player1} turn"
+      puts 'Pick any avaliable number 1 to 9'
+      board
+      player1_turn = gets.chomp
 
-loop do
-  puts "#{player1} turn"
-  puts 'Pick any avaliable number 1 to 9'
-  board
-  player1_turn = gets.chomp
-  player1_turns << player1_turn
+      puts "#{@player2} turn"
+      puts 'Pick any avaliable number 1 to 9'
+      board
+      player2_turn = gets.chomp
+    end
+  end
 
-  puts "#{player2} turn"
-  puts 'Pick any avaliable number 1 to 9'
-  board
-  player2_turn = gets.chomp
-  player2_turns << player2_turn
-end
-puts "#{player1} win!"
-puts "#{player2} win!"
-puts "#{player1} &  #{player2} draw"
-
-def start
+  def start
     welcome
+    play
+  end
 end
-
-end
-
-
 
 game = TicTacToe.new
 game.start
