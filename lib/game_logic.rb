c5a3afd9e899333c6483dd2f74d2d57a06e0bf1e
Yigit Mersin <yigitmersin@gmail.com>
2020-07-29 11:52:52 +0300

    Add game winner conditions to game_logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -4,13 +4,15 @@ module GameLogic
   @@player1_turns = []
   @@player2_turns = []
   @@winning_possibilities = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-
+  @@win = false
   def test_winner
     @@winning_possibilities.each do |possible_win|
       puts "#{@player1} win" if possible_win.difference(@@player1_turns) == []
+      @@win=true if possible_win.difference(@@player1_turns) == []
     end
     @@winning_possibilities.each do |possible_win|
       puts "#{@player2} win" if possible_win.difference(@@player2_turns) == []
+      @@win=true if possible_win.difference(@@player1_turns) == []
     end
     end
 end
