a3cb36ffe09fe541c187c5bf87a882760269e63d
jacobrees <jacob.rees97@yahoo.com>
2020-08-06 14:34:55 +0100

    Set up Rspec within the project create test cases for test_move and calculate_taken_turns methods

---
+++ spec/main_spec.rb
@@ -0,0 +1,37 @@
+require 'rspec'
+require './lib/game_logic.rb'
+
+describe GameLogic do
+  include GameLogic
+  describe '#test_move' do
+    it "returns true if given an intergure between 1 & 9 that isn't included inside taken turns array" do
+      @taken_turns = [1, 2, 3]
+      expect(test_move(4)).to be true
+    end
+
+    it 'returns false if given an intergure between 1 & 9 that is included inside taken turns array' do
+      @taken_turns = [1, 2, 3]
+      expect(test_move(3)).to be false
+    end
+
+    it 'returns false if given an intergure larger than 9' do
+      expect(test_move(10)).to be false
+    end
+
+    it 'returns false if given an intergure lower than 1' do
+      expect(test_move(-1)).to be false
+    end
+
+    it 'returns false if given a string' do
+      expect(test_move('string')).to be false
+    end
+  end
+
+  describe '#calculate_taken_turns' do
+    it 'returns the @takenturns array that is equal to @player1_turns + @player2_turns' do
+        @player1_turns = [1,2]
+        @player2_turns = [3,4]
+        expect(calculate_taken_turns).to eql([1,2,3,4])
+    end
+end
+end
